c44916618d4e829432e33a78feaf85c9979f0385
Saniewski <pawel.san16@gmail.com>
2023-03-23 01:24:43 +0100

    Implement fetcher for Reddit API

--- Golang/zadania/reddit/fetcher/redditFetcher.go
+++ Golang/zadania/reddit/fetcher/redditFetcher.go
@@ -1,7 +1,12 @@
 package fetcher
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
+	"time"
 )
 
 type response struct {
@@ -16,6 +21,56 @@ type response struct {
 }
 
 type RedditFetcher interface {
-	Fetch() error
+	Fetch(ctx context.Context) error
 	Save(io.Writer) error
+}
+
+type Fetcher struct {
+	response response
+	URL      string
+}
+
+func (g *Fetcher) Fetch(ctx context.Context) error {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, g.URL, nil)
+	if err != nil {
+		return fmt.Errorf("error while creating request: %v", err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error while fetching data: %v", err)
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("error while fetching data: %v", resp.StatusCode)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&g.response); err != nil {
+		return fmt.Errorf("error while decoding data: %v", err)
+	}
+
+	return nil
+}
+
+func (g *Fetcher) Save(w io.Writer) error {
+	_, err := fmt.Fprintf(w, "%s\n\nPOSTS:\n", time.Now())
+	if err != nil {
+		return fmt.Errorf("error while saving data: %v", err)
+	}
+	for _, post := range g.response.Data.Children {
+		_, err := fmt.Fprintf(w, "%s\n%s\n", post.Data.Title, post.Data.URL)
+		if err != nil {
+			return fmt.Errorf("error while saving data: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func NewFetcher(url string) *Fetcher {
+	return &Fetcher{
+		URL: url,
+	}
 }
--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -1,9 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"reddit/fetcher"
+	"sync"
+	"time"
 )
 
 func main() {
-	fmt.Println("hello r/golang!")
+	log.Println("Starting Reddit Fetcher...")
+	ufs := []urlAndFilename{
+		{"https://www.reddit.com/r/golang.json", "golang.txt"},
+		{"https://www.reddit.com/r/programmerhumor.json", "programmerhumor.txt"},
+		{"https://www.reddit.com/r/programminghorror.json", "programminghorror.txt"},
+		{"https://www.reddit.com/r/cscareerquestions.json", "cscareerquestions.txt"},
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(ufs))
+
+	for _, uf := range ufs {
+		go CreateAndRunFetcher(uf.url, uf.filename, &wg)
+	}
+
+	log.Println("Waiting for all fetchers to finish...")
+	wg.Wait()
+	log.Println("All fetchers finished.")
+}
+
+func CreateAndRunFetcher(url string, outputFilename string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var client fetcher.RedditFetcher
+	client = fetcher.NewFetcher(url)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := client.Fetch(ctx); err != nil {
+		log.Printf("error while fetching data: %v", err)
+	}
+
+	file, err := os.Create(outputFilename)
+	if err != nil {
+		log.Printf("error while creating file: %v", err)
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	if err = client.Save(file); err != nil {
+		log.Printf("error while saving data: %v", err)
+	}
+
+	log.Printf("Data from %s saved to %s", url, outputFilename)
+}
+
+type urlAndFilename struct {
+	url      string
+	filename string
 }
